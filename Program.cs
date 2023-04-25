da899a6baec9fc46c5afa84d7021e9204b6081a9
Pedro H. Alves <leonkene18@gmail.com>
2023-04-25 08:55:41 -0300

    a

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿static void BarraCarregamento(string texto, int Ponto, int Tempo)
+    {
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write(texto);
+
+        for (var i = 0; i < Ponto; i++)
+        {
+            Console.Write($".");
+            Thread.Sleep(Tempo);
+        }
+        Console.ResetColor();
+    }
+
+    BarraCarregamento("Mudando Perspectiva", 8, 300);
+    Console.WriteLine($"");
